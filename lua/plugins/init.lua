1585601e3f619235328f20002be5ea6c520c2dda
degumeds <degumeds@domesticandgeneral.com>
2024-01-04 02:51:29 +0200

    add keymaps for code hints

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -192,6 +192,10 @@ return {
 		config = function()
 			require("lspsaga").setup({})
 			vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga diagnostic_jump_next<cr>")
+			vim.keymap.set("n", "ga", "<cmd>Lspsaga code_action<cr>")
+			vim.keymap.set("n", "gf", "<cmd>Lspsaga finder<cr>")
+			vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>")
+			vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>")
 		end,
 	},
 	{
