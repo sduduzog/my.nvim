1ca7a892a232f615e1ac4e8ad0c9374e49b0a09e
degumeds <degumeds@domesticandgeneral.com>
2024-05-17 15:43:27 +0200

    update deps

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -427,19 +427,6 @@ return {
 		end,
 	},
 	{
-		"xiyaowong/transparent.nvim",
-		config = function()
-			require("transparent").setup({
-				"NormalFloat",
-				extra_groups = {
-					"NvimTreeNormal",
-					"ToggleTerm",
-				},
-			})
-			vim.keymap.set("n", "<S-F6>", ":TransparentToggle<CR>")
-		end,
-	},
-	{
 		"nvim-treesitter/nvim-treesitter",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
